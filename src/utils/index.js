804f31bea824291e2a444f7d45c3d22d4ebde658
Bruno Futema <bruno.futema@outlook.com>
2020-07-20 18:42:24 -0300

    feat(frontend): add cpfCnpj function in utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,3 +3,22 @@ export function onlyChars(value, cb) {
 
   if (re.test(value)) cb(value);
 }
+
+export function cpfCnpj(value, cb) {
+  value = value.replace(/\D/g, '');
+
+  if (value.length > 14) value = value.slice(0, -1);
+
+  if (value.length > 11) {
+    value = value.replace(/(\d{2})(\d)/, '$1.$2');
+    value = value.replace(/(\d{3})(\d)/, '$1.$2');
+    value = value.replace(/(\d{3})(\d)/, '$1/$2');
+    value = value.replace(/(\d{4})(\d)/, '$1-$2');
+  } else {
+    value = value.replace(/(\d{3})(\d)/, '$1.$2');
+    value = value.replace(/(\d{3})(\d)/, '$1.$2');
+    value = value.replace(/(\d{3})(\d)/, '$1-$2');
+  }
+
+  cb(value);
+}
